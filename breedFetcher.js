0475a2154348d2af1bccdcededf83d6f7fc88b1f
jad <jad_dis@hotmail.com>
2019-09-26 17:17:10 +0000

    added breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require("request");
 const args = process.argv.slice(2);
-function breedFetcher(breed) {
+const breedFetcher = (breed) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 
     if (error) {
       console.log(error);
-    }
-    else {
+    } else {
       const data = JSON.parse(body);
       if (data.length === 0) {
         console.log("Breed not found.");
-      }
-      else {
+      } else {
         console.log(data[0].description);
       }
     }
   });
-}
+};
 
 breedFetcher(args[0]);
