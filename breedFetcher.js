08ff5721010bdf34594db6a24e7a316f41b68314
jad <jad_dis@hotmail.com>
2019-09-26 17:42:24 +0000

    refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,16 @@
 const request = require("request");
 const args = process.argv.slice(2);
-const breedFetcher = (breed) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 
-    if (error) {
-      console.log(error);
+
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed not found.");
     } else {
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        console.log("Breed not found.");
-      } else {
-        console.log(data[0].description);
-      }
+      callback(data[0].description);
     }
   });
-};
+}
 
-breedFetcher(args[0]);
+module.exports = { fetchBreedDescription };
